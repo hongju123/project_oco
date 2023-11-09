dfb45addbca2e4f2de70102b17815b7b40e74b21
최 국현 <lij3336@naver.com>
2023-11-09 09:48:58 +0900

    상세페이지 파일 업로드까지만 구현

--- src/main/java/com/oco/domain/dto/BusinessInfoDTO.java
+++ src/main/java/com/oco/domain/dto/BusinessInfoDTO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class BusinessInfoDTO {
-	private Long businessInfoIdex;
+	private Long businessInfoIdx;
     private int visitCount;
     private String useTime;
     private String content;
